9ae75c63079f4c1baf5ef35c945624706ef6c23c
Oleksandr Hrabovets <alex6754035@gmail.com>
2024-01-21 11:12:36 +0200

    add init migration

---
+++ src/main/resources/db/migration/V1__create__users_roles.sql
@@ -0,0 +1,27 @@
+CREATE TABLE users
+(
+    id         BIGSERIAL PRIMARY KEY,
+    username   VARCHAR(100)  NOT NULL UNIQUE,
+    password   VARCHAR(1024) NOT NULL,
+    first_name VARCHAR(100)  NOT NULL,
+    last_name  VARCHAR(100)  NOT NULL,
+    enabled    BOOLEAN       NOT NULL DEFAULT FALSE,
+    email      VARCHAR(200)  NOT NULL,
+    created_at TIMESTAMP     NOT NULL,
+    updated_at TIMESTAMP     NOT NULL
+);
+
+CREATE TABLE roles
+(
+    id   BIGSERIAL PRIMARY KEY,
+    name VARCHAR(100) NOT NULL UNIQUE
+);
+
+CREATE TABLE users_roles
+(
+    id      BIGSERIAL PRIMARY KEY,
+    user_id BIGINT NOT NULL,
+    role_id BIGINT NOT NULL,
+    FOREIGN KEY (user_id) REFERENCES users (id),
+    FOREIGN KEY (role_id) REFERENCES roles (id)
+);
