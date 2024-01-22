a9790b69f7f69158dd8aabb58de900eca36cb1d3
Oleksandr Hrabovets <alex6754035@gmail.com>
2024-01-22 18:40:19 +0200

    fix migration

--- src/main/resources/db/migration/V1__create__users_roles.sql
+++ src/main/resources/db/migration/V1__create__users_roles.sql
@@ -13,8 +13,10 @@ CREATE TABLE users
 
 CREATE TABLE roles
 (
-    id   BIGSERIAL PRIMARY KEY,
-    name VARCHAR(100) NOT NULL UNIQUE
+    id         BIGSERIAL PRIMARY KEY,
+    name       VARCHAR(100) NOT NULL UNIQUE,
+    created_at TIMESTAMP    NOT NULL,
+    updated_at TIMESTAMP    NOT NULL
 );
 
 CREATE TABLE users_roles
